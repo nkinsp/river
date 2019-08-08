e62bd0e6517ddd2ef94aa83225d03c0b64020dd2
unknown <yue571041524@qq.com>
2019-08-08 16:09:26 +0800

    init

---
+++ route.go
@@ -0,0 +1,27 @@
+package hummer
+
+
+
+type IRoute interface {
+
+	Any(path string,methods []string,handler HanderFun) IRoute
+	Get(path string,handler HanderFun) IRoute
+	Post(path string,handler HanderFun) IRoute
+	Put(path string,handler HanderFun)IRoute
+	Delete(path string,handler HanderFun)IRoute
+
+}
+
+type Router struct {
+
+}
+
+func (router *Router) Any(path string,method []string,handler HanderFun) Router {
+
+	return  *router;
+}
+
+
+
+
+
