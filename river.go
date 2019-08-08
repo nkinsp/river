5d7b48e85a3895271e4fb86d05a79a55068ca40b
unknown <yue571041524@qq.com>
2019-08-08 16:12:44 +0800

    init

--- river.go
+++ river.go
@@ -1,1 +1,39 @@
 package river
+
+type Request struct {
+
+}
+type HanderFun func()
+
+type RequestBody interface {
+
+}
+
+type App struct {
+
+	Router Router
+
+}
+
+type RouterFun func(router Router)
+
+func (app *App) ConfigRouter(fun RouterFun)  {
+
+
+}
+
+
+
+
+func Create() int  {
+
+	app := App{Router{}}
+
+	app.ConfigRouter(func(router Router) {
+		router.Any("",[]string{"",""}, func() {
+
+		})
+	})
+
+	return 0
+}
--- route.go
+++ route.go
@@ -1,4 +1,4 @@
-package hummer
+package river
 
 
 
